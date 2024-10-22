60389b712e5b10ebdad8c41a51708ca97984f5ff
pcokis <prestoncokis@gmail.com>
2024-10-21 20:16:29 -0400

    Added BSD-3 Clause

--- lib/src/main/kotlin/org/fotmrobotics/trailblazer/Math.kt
+++ lib/src/main/kotlin/org/fotmrobotics/trailblazer/Math.kt
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2024, Fire on the Mountain Robotics
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
 package org.fotmrobotics.trailblazer
 
 fun angleWrap (angle: Double) =
--- lib/src/main/kotlin/org/fotmrobotics/trailblazer/PIDF.kt
+++ lib/src/main/kotlin/org/fotmrobotics/trailblazer/PIDF.kt
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2024, Fire on the Mountain Robotics
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
 package org.fotmrobotics.trailblazer
 
 class PIDF (private var kP: Double, private var kI: Double, private var kD: Double, private var kF: Double) {
