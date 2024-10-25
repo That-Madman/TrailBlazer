b0e2be3827501cda87e95eea627ae045f7aad6aa
pcokis <prestoncokis@gmail.com>
2024-10-25 17:20:11 -0400

    changes some stuff with vector2d

--- lib/src/main/kotlin/org/fotmrobotics/trailblazer/Vector.kt
+++ lib/src/main/kotlin/org/fotmrobotics/trailblazer/Vector.kt
@@ -14,23 +14,23 @@ import kotlin.math.PI
 import kotlin.math.pow
 
 open class Vector2D (var x: Double, var y: Double) {
-    operator fun plus(v: Vector2D) = Vector2D(x + v.x, y + v.y)
+    operator fun plus (v: Vector2D) = Vector2D(x + v.x, y + v.y)
 
     operator fun inc() = Vector2D(x + 1, y + 1)
 
-    operator fun minus(v: Vector2D) = Vector2D(x - v.x, y - v.y)
+    operator fun minus (v: Vector2D) = Vector2D(x - v.x, y - v.y)
 
     operator fun dec() = Vector2D(x - 1, y - 1)
 
-    operator fun times(value: Double) = Vector2D(x * value, y * value)
+    operator fun times (value: Double) = Vector2D(x * value, y * value)
 
-    operator fun times(v: Vector2D) = Vector2D(x * v.x, y * v.y)
+    operator fun times (v: Vector2D) = Vector2D(x * v.x, y * v.y)
 
-    operator fun div(value: Double) = Vector2D(x / value, y / value)
+    operator fun div (value: Double) = Vector2D(x / value, y / value)
 
-    operator fun rem(value: Double) = Vector2D(x % value, y % value)
+    operator fun rem (value: Double) = Vector2D(x % value, y % value)
 
-    open operator fun get(i: Int): Double {
+    open operator fun get (i: Int): Double {
         return when (i) {
             0 -> x
             1 -> y
@@ -38,7 +38,7 @@ open class Vector2D (var x: Double, var y: Double) {
         }
     }
 
-    open operator fun set(i: Int, value: Double) {
+    open operator fun set (i: Int, value: Double) {
         when (i) {
             0 -> {x = value}
             1 -> {y = value}
@@ -57,7 +57,7 @@ open class Vector2D (var x: Double, var y: Double) {
         this.y = vector2d.y
     }
 
-    operator fun compareTo(v: Vector2D): Int {
+    operator fun compareTo (v: Vector2D): Int {
         return when {
             this.norm() > v.norm() -> 1
             this.norm() < v.norm() -> -1
@@ -65,15 +65,15 @@ open class Vector2D (var x: Double, var y: Double) {
         }
     }
 
-    infix fun dot(v: Vector2D) = x * v.x + y * v.y
+    infix fun dot (v: Vector2D) = x * v.x + y * v.y
 
-    infix fun x(v: Vector2D) = x*v.y - y*v.x
+    infix fun x (v: Vector2D) = x*v.y - y*v.x
 
-    infix fun proj(v: Vector2D) = v * (this dot v) / (v dot v)
+    infix fun proj (v: Vector2D) = v * (this dot v) / (v dot v)
 
     operator fun unaryMinus() = Vector2D(-x, -y)
 
-    fun pow(n: Int) = Vector2D(x.pow(n), y.pow(n))
+    fun pow (n: Int) = Vector2D(x.pow(n), y.pow(n))
 
     fun sum() = x + y
 
@@ -82,8 +82,8 @@ open class Vector2D (var x: Double, var y: Double) {
     fun angle() = angleWrap(atan2(y, x) * 180 / PI)
 }
 
-class Pose2D(x: Double, y: Double, var h: Double): Vector2D(x, y) {
-    override operator fun get(i: Int): Double {
+class Pose2D (x: Double, y: Double, var h: Double): Vector2D(x, y) {
+    override operator fun get (i: Int): Double {
         return when (i) {
             0 -> x
             1 -> y
@@ -92,7 +92,7 @@ class Pose2D(x: Double, y: Double, var h: Double): Vector2D(x, y) {
         }
     }
 
-    override operator fun set(i: Int, value: Double) {
+    override operator fun set (i: Int, value: Double) {
         when (i) {
             0 -> {x = value}
             1 -> {y = value}
