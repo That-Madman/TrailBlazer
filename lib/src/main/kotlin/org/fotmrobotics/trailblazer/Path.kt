b6ebe0aeefb2672101f233acbd03568d12e787f4
pcokis <prestoncokis@gmail.com>
2024-11-09 22:18:16 -0500

    i think this fix it

--- lib/src/main/kotlin/org/fotmrobotics/trailblazer/Path.kt
+++ lib/src/main/kotlin/org/fotmrobotics/trailblazer/Path.kt
@@ -9,7 +9,7 @@ LICENSE file in the root directory of this source tree.
 package org.fotmrobotics.trailblazer
 
 fun driveVector (spline: Spline, pos: Pose2D, pidf: PIDF): Pose2D {
-    val t = spline.getClosestPoint(pos)
+    val t = spline.getClosest(pos)
 
     val splinePoint = spline.getPoint(t)
     val splineDeriv = spline.getDeriv(t)
