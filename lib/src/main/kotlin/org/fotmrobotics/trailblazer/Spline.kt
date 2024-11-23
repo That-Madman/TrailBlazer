157efc7c8e370080855a52ebd2e56136668d904a
pcokis <prestoncokis@gmail.com>
2024-11-22 20:28:11 -0500

    fix?

--- lib/src/main/kotlin/org/fotmrobotics/trailblazer/Spline.kt
+++ lib/src/main/kotlin/org/fotmrobotics/trailblazer/Spline.kt
@@ -113,7 +113,7 @@ class Spline() {
 
     fun getSegmentPoints(): ArrayList<Vector2D> {
         val points = ArrayList<Vector2D>()
-        for (i in 0..3) {points.add(controlPoints[segment+i-1])}
+        for (i in 0..3) {points.add(controlPoints[segment+i])}
         return points
     }
 
