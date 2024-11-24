d4a42a7244de622797e5914a0630e49d49dd5af7
pcokis <prestoncokis@gmail.com>
2024-11-23 20:22:50 -0500

    removed test function

--- lib/src/main/kotlin/org/fotmrobotics/trailblazer/Path.kt
+++ lib/src/main/kotlin/org/fotmrobotics/trailblazer/Path.kt
@@ -19,7 +19,7 @@ fun driveVector (spline: Spline, pos: Pose2D, pidf: PIDF): Pose2D {
     val splinePoint = spline.getPoint(t)
     val splineDeriv = spline.getDeriv(t)
 
-    var angle = atan2(splineDeriv.y, splineDeriv.x) % (2 * Math.PI)
+    var angle = atan2(splineDeriv.y, splineDeriv.x)
     //if (splineDeriv.x < 0) angle += Math.PI
     //var angle = atan2(splineDeriv.y, splineDeriv.x)
     //val splineDeriv2 = spline.getDeriv2(t)
@@ -40,22 +40,6 @@ fun driveVector (spline: Spline, pos: Pose2D, pidf: PIDF): Pose2D {
     val drive = forward + translation + centripetal
 
     return Pose2D(drive.x, drive.y, angle)
-}
-
-fun test(spline: Spline, pos: Pose2D, pidf: PIDF): Pose2D {
-    val t = spline.getClosestPoint(pos)
-    val splineDeriv = spline.getDeriv(t)
-    var angle = atan2(splineDeriv.y, splineDeriv.x) % (2 * Math.PI)
-    if (splineDeriv.x < 0) angle += Math.PI
-    val forward = splineDeriv
-    val k = curvature(spline, t)
-    val centripetalMagnitude = forward.norm().pow(2.0) * k
-    val centripetal = Vector2D(
-        centripetalMagnitude * cos(angle + Math.PI / 2),
-        centripetalMagnitude * sin(angle + Math.PI / 2)
-    )
-
-    return Pose2D(centripetal.x, centripetal.y, k)
 }
 
 /*class PathSegment () {
