9bc363e1cc6820298154b8352708ced7c99c7861
pcokis <prestoncokis@gmail.com>
2024-11-23 19:48:36 -0500

    Test

--- lib/src/main/kotlin/org/fotmrobotics/trailblazer/Path.kt
+++ lib/src/main/kotlin/org/fotmrobotics/trailblazer/Path.kt
@@ -39,7 +39,23 @@ fun driveVector (spline: Spline, pos: Pose2D, pidf: PIDF): Pose2D {
 
     val drive = forward + translation + centripetal
 
-    return Pose2D(drive.x, drive.y, (Math.toDegrees(angle) + 90) % 360)
+    return Pose2D(drive.x, drive.y, angle)
+}
+
+fun test(spline: Spline, pos: Pose2D, pidf: PIDF): Pose2D {
+    val t = spline.getClosestPoint(pos)
+    val splineDeriv = spline.getDeriv(t)
+    var angle = atan2(splineDeriv.y, splineDeriv.x) % (2 * Math.PI)
+    if (splineDeriv.x < 0) angle += Math.PI
+    val forward = splineDeriv
+    val k = curvature(spline, t)
+    val centripetalMagnitude = forward.norm().pow(2.0) * k
+    val centripetal = Vector2D(
+        centripetalMagnitude * cos(angle + Math.PI / 2),
+        centripetalMagnitude * sin(angle + Math.PI / 2)
+    )
+
+    return Pose2D(centripetal.x, centripetal.y, k)
 }
 
 /*class PathSegment () {
